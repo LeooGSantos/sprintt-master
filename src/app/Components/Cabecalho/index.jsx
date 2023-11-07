6a68686db79743fe15f9ce7aecce03c9c75f9d58
LeooGSantos <rm98912@fiap.com.br>
2023-11-07 09:25:30 -0300

    cabecalho

--- src/app/Components/Cabecalho/index.jsx
+++ src/app/Components/Cabecalho/index.jsx
@@ -4,6 +4,35 @@ export default function Cabecalho() {
   return (
     <header className="cabecalho">
       <h2>MEUS PRODUTOS</h2>
+      <nav>
+        <ul>
+          <li>
+            <Link href="/bike">
+              <button>Bike</button>
+            </Link>
+          </li>
+          <li>
+            <Link href="/fazerseguro">
+              <button>Fazer Seguro</button>
+            </Link>
+          </li>
+          <li>
+            <Link href="/planosseguro">
+              <button>Planos de Seguro</button>
+            </Link>
+          </li>
+          <li>
+            <Link href="/principal">
+              <button>Principal</button>
+            </Link>
+          </li>
+          <li>
+            <Link href="/rms">
+              <button>RMs</button>
+            </Link>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 }
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,4 @@
 import Link from 'next/link';
-import Cabecalho from './Components/Cabecalho/index';
 
 export default function Home() {
   return (
@@ -7,7 +6,7 @@ export default function Home() {
       <h1>Seguro Bike da Porto</h1>
       <h2>Seguro</h2>
       <p>Uma solução completa que oferece proteção e serviços para você se aventurar sem medo.</p>
-      <Link href="/PlanosSeguro">
+      <Link href="/PlanosSeguro"> 
         <button>Cote já</button>
       </Link>
       <p>Com o Seguro Bike da Porto Seguro, é pedalar e relaxar.</p>
