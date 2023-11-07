d208cad7c9da9b66ff7682414d71e8ad2800c3e1
LeooGSantos <rm98912@fiap.com.br>
2023-11-07 09:04:33 -0300

    Cabecalho

---
+++ src/app/Components/Cabecalho/index.jsx
@@ -0,0 +1,9 @@
+import Link from "next/link";
+
+export default function Cabecalho() {
+  return (
+    <header className="cabecalho">
+      <h2>MEUS PRODUTOS</h2>
+    </header>
+  );
+}
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,17 @@
-import Image from 'next/image'
+import Link from 'next/link';
+import Cabecalho from './Components/Cabecalho/index';
 
 export default function Home() {
   return (
-    <p>Hello</p>
-  )
+    <div className="pagina-principal">
+      <h1>Seguro Bike da Porto</h1>
+      <h2>Seguro</h2>
+      <p>Uma solução completa que oferece proteção e serviços para você se aventurar sem medo.</p>
+      <Link href="/PlanosSeguro">
+        <button>Cote já</button>
+      </Link>
+      <p>Com o Seguro Bike da Porto Seguro, é pedalar e relaxar.</p>
+      <p>Faça o seguro agora!!</p>
+    </div>
+  );
 }
