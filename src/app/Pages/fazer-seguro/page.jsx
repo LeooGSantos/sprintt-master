8caa6d3368ece1b74a50068e3189ac87ad0bc71f
LeooGSantos <rm98912@fiap.com.br>
2023-11-13 08:40:36 -0300

    Página fazer seguro

--- src/app/Pages/fazer-seguro/page.jsx
+++ src/app/Pages/fazer-seguro/page.jsx
@@ -1,19 +1,43 @@
-import Link from 'next/link';
+// @react
+import React from 'react';
 
-export default function Fazer() {
+export default function FazerSeguro() {
   return (
-    <div className="bike-page">
-      <h1>Fazer Seguro</h1>
-      <p>Faça seu seguro agora, de forma dinâmica e rápida!</p>
-      <button>
-        <Link href="/Pages/fazer-seguro">Fazer Seguro</Link>
-      </button>
+    <div className="fazer-seguro-page">
+      <div className="lado-esquerdo">
+        <h2>Seguro Bike da Porto</h2>
+        <p>Seguro</p>
+      </div>
+      <div className="lado-direito">
+        <form>
+          <label htmlFor="marca">Marca</label>
+          <input type="text" id="marca" />
 
-      <h2>Ver Seus Seguros de Bike</h2>
-      <p>Veja seus seguros aqui!</p>
-      <button>
-        <Link href="/Pages/planos-seguro">Planos Seguro</Link>
-      </button>
+          <label htmlFor="modelo">Modelo</label>
+          <input type="text" id="modelo" />
+
+          <label htmlFor="preco">Preço</label>
+          <input type="text" id="preco" />
+
+          <label htmlFor="chassi">Número do Chassi (Número de série)</label>
+          <input type="text" id="chassi" />
+
+          <button type="button">Confirmar Número do Chassi</button>
+
+          <label htmlFor="tempoPossui">Tempo que Possui a Bicicleta</label>
+          <input type="text" id="tempoPossui" />
+
+          <label htmlFor="acessorios">Acessórios</label>
+          <input type="text" id="acessorios" />
+
+          <label htmlFor="uso">Uso da Bike</label>
+          <input type="text" id="uso" />
+
+          <p>Ao clicar em "Continuar" você confirma os dados.</p>
+
+          <button type="submit">Continuar</button>
+        </form>
+      </div>
     </div>
   );
 }
