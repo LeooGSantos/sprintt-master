292d758439deb0eb3ede46fd969646be10ccc2a4
LeooGSantos <rm98912@fiap.com.br>
2023-11-13 23:22:10 -0300

    Algumas alterações

--- src/Components/Rodape/index.jsx
+++
@@ -1,9 +0,0 @@
-export default function Rodape() {
-    return (
-      <footer className="rodape">
-        <div>
-          <p>Desenvolvido por SafeCompany</p>
-        </div>
-      </footer>
-    );
-  }
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <div className="pagina-principal">
       <h1>Seguro de bicicletas da Porto</h1>
+      <img src="/img/5p.jpg" alt="Imagem2" className="imagem-plano" />
       <p>Uma solução completa que oferece proteção e serviços para você se aventurar sem medo.</p>
       <p>Com o Seguro Bike da Porto Seguro, é pedalar e relaxar.</p>
       <p>Faça seu seguro agora, de forma dinâmica e rápida!</p>
