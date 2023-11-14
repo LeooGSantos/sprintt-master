98f2ec95a687725e0ba1b5626884615812d012f3
ErikTeixeira <erik.barbosa515@gmail.com>
2023-11-13 22:21:16 -0300

    atualizacao

--- src/app/Pages/planos-seguro/page.jsx
+++ src/app/Pages/planos-seguro/page.jsx
@@ -8,12 +8,12 @@ export default function PlanosSeguro() {
         <img src="/img/2p.jpg" alt="Imagem1" className="imagem-plano" />
         <p>O plano gratuito* para você experimentar um dos serviços essenciais oferecidos, de acordo com as suas necessidades.</p>
         <button>
-          <Link href="/Pages/fazer-seguro">Faça já</Link>
+          <Link href="/Pages/fazer-seguro">Faça já!</Link>
         </button>
       </div>
 
       <div className="quadrado">
-        <h2>Pedal Leve</h2>
+        <h2>- Pedal Leve -</h2>
         <img src="/img/4p.jpg" alt="Imagem2" className="imagem-plano" />
         <p>Você gosta de pedalar e está buscando um plano de serviços intermediário? O Pedal leve da Porto é para você!</p>
         <button>
@@ -26,7 +26,7 @@ export default function PlanosSeguro() {
         <img src="/img/3p.jpg" alt="Imagem3" className="imagem-plano" />
         <p>Conte com diversos serviços capazes de elevar suas aventuras para o próximo nível.</p>
         <button>
-          <Link href="/Pages/fazer-seguro">Faça já</Link>
+          <Link href="/Pages/fazer-seguro">Faça já!</Link>
         </button>
       </div>
     </div>
