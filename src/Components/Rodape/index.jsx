af5eea9324cbc13c7a483a69510a21e65f048f82
AlleSilvaa <allessonaugusto10@gmail.com>
2023-11-13 22:55:08 -0300

    reformulando rodape

---
+++ src/Components/Rodape/index.jsx
@@ -0,0 +1,9 @@
+export default function Rodape() {
+    return (
+      <footer className="rodape">
+        <div>
+          <p>Desenvolvido por SafeCompany</p>
+        </div>
+      </footer>
+    );
+  }
