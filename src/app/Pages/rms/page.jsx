b1c12b8641db91a411dd69ef0b13d840bcb15ea9
LeooGSantos <rm98912@fiap.com.br>
2023-11-13 22:21:56 -0300

    Correções

--- src/app/Pages/rms/page.jsx
+++ src/app/Pages/rms/page.jsx
@@ -5,25 +5,26 @@ export default function RMs() {
       <ul>
         <li>
           <strong>
-            <img src="caminho/para/sua/imagem1.jpg" alt="Imagem1" className="imagem-rm" />
             RM99533 - Allesson Augusto Silva Perru Oliveira
           </strong>     
         </li>
         <li>
           <strong>
-            <img src="caminho/para/sua/imagem2.jpg" alt="Imagem2" className="imagem-rm" />
+            RM551377 - Erik Teixeira barbosa
+          </strong>        
+        </li>
+        <li>
+          <strong>
             RM552178 - Cauã Mongs de Borba
           </strong>        
         </li>
         <li>
           <strong>
-            <img src="caminho/para/sua/imagem3.jpg" alt="Imagem3" className="imagem-rm" />
             RM551456 - Guilherme Naoki Hirayama
           </strong>         
         </li>
         <li>
           <strong>
-            <img src="caminho/para/sua/imagem4.jpg" alt="Imagem4" className="imagem-rm" />
             RM98912 - Leonardo Gonçalves da Costa Santos
           </strong>
         </li>
